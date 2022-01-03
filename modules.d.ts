2d411f87096172c51ef9405a4b2cd14434c63081
CapsAdmin <eliashogstvedt@gmail.com>
2022-01-03 11:09:41 +0100

    fix imports of other assets not working in gui-theme

--- modules.d.ts
+++ modules.d.ts
@@ -3,6 +3,8 @@
 /// <reference types="react-dom/experimental" />/// <reference types="react/experimental" />
 
 declare module "*.png"
+declare module "*.jpg"
+declare module "*.jpeg"
 declare module "*.svg"
 declare module "*.mp3"
 declare module "*.ogg"
