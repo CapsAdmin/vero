0f578c119cc7b1da08ecb7f895bbbd572a4f1dd9
CapsAdmin <eliashogstvedt@gmail.com>
2023-06-21 17:08:38 +0200

    upgrade all packages

--- packages/client/esbuild.mjs
+++ packages/client/esbuild.mjs
@@ -1,6 +1,5 @@
 import esbuild from "esbuild"
 import fs from "fs-extra"
-import serve, { error, log } from "create-serve"
 import { spawn } from "child_process"
 
 const tsc = () => {
@@ -12,58 +11,53 @@ const tsc = () => {
 }
 
 console.time("build")
-esbuild
-	.build({
-		entryPoints: ["src/index.tsx"],
-		outfile: "public/bundle.js",
-		bundle: true,
-		minify: false,
-		sourcemap: "both",
-		define: {
-			"process.env.NODE_ENV": '"development"',
-			"process.env.JEST_WORKER_ID": "undefined",
-			"process.env.PUBLIC_URL": '"http://localhost"',
-		},
-		loader: {
-			".png": "dataurl",
-			".jpg": "dataurl",
-			".ttf": "dataurl",
-			".svg": "dataurl",
-		},
-		plugins: [
-			{
-				name: "map react-native",
-				setup(build) {
-					build.onLoad({ filter: /react\-native\/index\.js/ }, async (args) => {
-						return {
-							loader: "js",
-							resolveDir: "./node_modules/react-native-web/dist/",
-							contents: await fs.promises.readFile("./node_modules/react-native-web/dist/index.js"),
-						}
-					})
-				},
-			},
-		],
-		watch: {
-			onRebuild(err) {
-				tsc()
-				serve.update()
-				err ? error("× Failed") : log("✓ Updated")
+const context = await esbuild.context({
+	entryPoints: ["src/index.tsx"],
+	outfile: "public/bundle.js",
+	bundle: true,
+	minify: false,
+	target: "ES2020",
+	sourcemap: "both",
+	define: {
+		"process.env.NODE_ENV": '"development"',
+		"process.env.JEST_WORKER_ID": "undefined",
+		"process.env.PUBLIC_URL": '"http://localhost"',
+	},
+	loader: {
+		".png": "dataurl",
+		".jpg": "dataurl",
+		".ttf": "dataurl",
+		".svg": "dataurl",
+	},
+	plugins: [
+		{
+			name: "map react-native",
+			setup(build) {
+				build.onLoad({ filter: /react\-native\/index\.js/ }, async (args) => {
+					return {
+						loader: "js",
+						resolveDir: "./node_modules/react-native-web/dist/",
+						contents: await fs.promises.readFile("./node_modules/react-native-web/dist/index.js"),
+					}
+				})
 			},
 		},
-	})
-	.catch(() => process.exit(1))
-	.then(() => {
-		console.timeEnd("build")
-	})
-fs.emptyDirSync("./public/assets/")
-fs.copySync("../gui/src/assets/", "./public/assets/")
+	],
+})
+
+const result = await context.rebuild()
+console.timeEnd("build")
 
 tsc()
 
-serve.start({
+fs.emptyDirSync("./public/assets/")
+fs.copySync("../gui/src/assets/", "./public/assets/")
+
+await context.watch()
+
+ await context.serve({
 	port: 3000,
-	root: "./public",
-	index: "index.html",
-	fallback: "index.html",
-})
+	servedir: "./public",
+ })
+
+ console.log("http://localhost:3000")
